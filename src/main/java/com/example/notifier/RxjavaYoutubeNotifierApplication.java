ee76978577455d203bbef4a146bb9293c9035bd8
DanielAkkina <danielakkina@gmail.com>
2025-09-26 15:32:16 +0530

    chore(init): Spring Boot project scaffold

---
+++ src/main/java/com/example/notifier/RxjavaYoutubeNotifierApplication.java
@@ -0,0 +1,13 @@
+package com.example.notifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RxjavaYoutubeNotifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RxjavaYoutubeNotifierApplication.class, args);
+	}
+
+}
